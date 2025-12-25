609224a988c096ad7cd65c4429a3a98aa2e70eb9
GorujoCY <gorupublic@gorujokun.cy>
2025-12-25 21:05:46 +0100

    Upload files to "modules/prerequisites"

--- modules/prerequisites/client.py
+++ modules/prerequisites/client.py
@@ -9,12 +9,21 @@ class AdbCheckClient:
                 os.chdir(adb_is_true[1])
                 if s_os == "Windows":
                     adb_c_slash_d_check_cmd = subprocess.run(['adb', 'devices'], capture_output=True).stdout
-                    if 'unauthorized' in adb_c_slash_d_check_cmd:
-                        pass #just like below basically
+
                 elif s_os.endwith("Linux") or s_os == "Linux":
                     adb_c_slash_d_check_cmd = subprocess.run(['./adb', 'devices'], capture_output=True).stdout
-                    if 'unauthorized' in adb_c_slash_d_check_cmd:
-                        pass #just like below basically
+                    while 'unauthorized' in adb_c_slash_d_check_cmd
+                        print("Please check a guide on how to turn on developer options on your device and go to it. Press enter when you're done! (you will get the same prompts if you didnt authorize the device)")
+                        input()
+                        print("Now enable USB Debugging and Authorize your computer (DONT FORGET to also tick Always allow from this computer for a more convenient experience)! \nWARNING: Xiaomi prevents install via usb by default unless you make a xiaomi account or do workarounds (required for app installations to work), search for that same guide on how to bypass it or make a temporary account! It is a frustrating limitation that Xiaomi implements that is out of our control.")
+                        input()
+                        print("Trying again...")
+                        adb_c_slash_d_check_cmd = subprocess.run(['./adb', 'devices'], capture_output=True).stdout
+                        if 'unauthorized' in adb_c_slash_d_check_cmd:
+                            continue
+                        else:
+                            break
+
         elif isinstance(adb_is_true, bool)
             if adb_is_true:
                 adb_c_slash_d_check_cmd = subprocess.run(['adb', 'devices'], capture_output=True).stdout
@@ -37,19 +46,22 @@ class AdbCheckClient:
                         continue
 
 
-    def check_phone_manufacturer(adb_is_true, s_os, adbc):
+    def check_phone_manufacturer(adb_is_true, is_device_trusted, s_os, adbc):
         if isinstance(adb_is_true, list):
             if adb_is_true[0]:
-                os.chdir(adb_is_true[1])
-                if s_os == "Windows":
-                    c_manufacturer = subprocess.run(['adb', 'shell', 'getprop', 'ro.product.manufacturer'], capture_output=True)
-                elif s_os.endwith("Linux") or s_os == "Linux":
-                    c_manufacturer = subprocess.run('./adb', 'shell', 'getprop', 'ro.product.manufacturer'], capture_output=True)}
+                if is_device_trusted:
+                    os.chdir(adb_is_true[1])
+                    if s_os == "Windows":
+                        c_manufacturer = subprocess.run(['adb', 'shell', 'getprop', 'ro.product.manufacturer'], capture_output=True)
+                    elif s_os.endwith("Linux") or s_os == "Linux":
+                        c_manufacturer = subprocess.run('./adb', 'shell', 'getprop', 'ro.product.manufacturer'], capture_output=True)
         else:
             if isinstance(adb_is_true, bool)
                 if adb_is_true:
-                    c_manufacturer = subprocess.run(['adb', 'shell', 'getprop', 'ro.product.manufacturer'], capture_output=True)
+                    if is_device_trusted
+                        c_manufacturer = subprocess.run(['adb', 'shell', 'getprop', 'ro.product.manufacturer'], capture_output=True)
             else:
                 if adb_is_true == "built-in":
-                    c_manufacturer = adbc.prop.get("ro.product.manufacturer")
+                    if is_device_trusted
+                        c_manufacturer = adbc.prop.get("ro.product.manufacturer")
         return c_manufacturer
