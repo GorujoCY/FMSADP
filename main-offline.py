353021d9254ffe2ce61afe90c791a7e2dd7f3cd2
GorujoCY <gorupublic@gorujokun.cy>
2025-12-30 00:33:51 +0100

    forgot built in users...

--- main-offline.py
+++ main-offline.py
@@ -11,6 +11,7 @@ checked_adb = AdbCheckComputer.check_adb(computer_os)
 
 adb_device = AdbCheckClient.authorize_device_adb(checked_adb, computer_os)
 
+
 #START Yes/No question 1
 print('Replace Play Store with Aurora Store?')
 playstore_to_aurora_yn = ''
@@ -44,12 +45,19 @@ while install_browser_choice not in (1, 2):
 #END Browser question
 
 
-
 #now begins the main bits
-
 #installing
 if checked_adb == 'built-in':
     AdbInstallApps.install_apps('offline', checked_adb, computer_os, 'bundled_apks', adb_device)
+    if install_browser_choice == 1:
+        AdbInstallApps.install_apps('offline', checked_adb, computer_os, 'bundled_apks/browsers/brave', adb_device)
+    elif install_browser_choice == 2:
+        AdbInstallApps.install_apps('offline', checked_adb, computer_os, 'bundled_apks/browsers/ironfox', adb_device)
+    #Google Suite to Proton suite
+    if google_suite_to_proton_yn.upper() in ('Y', 'YES'):
+        AdbInstallApps.install_apps('offline', checked_adb, computer_os, 'bundled_apks/when_consented_by_user/proton_suite', adb_device)
+    if playstore_to_aurora_yn.upper() in ('Y', 'YES'):
+        AdbInstallApps.install_apps('offline', checked_adb, computer_os, 'bundled_apks/when_consented_by_user/aurora_store', adb_device)
 else:
     AdbInstallApps.install_apps('offline', checked_adb, computer_os, 'bundled_apks')
     #browser choice
