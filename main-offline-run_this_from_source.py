164aa18882f0078bbe76c2214a126d777e37e1a3
GorujoCY <gorupublic@gorujokun.cy>
2026-01-02 18:50:32 +0100

    Update main-offline-run_this_from_source.py

--- main-offline-run_this_from_source.py
+++ main-offline-run_this_from_source.py
@@ -71,8 +71,8 @@ clear_screen()
 
 #since this is a run from source thing lets make it that it gets the zip and extracts
 urllib.request.urlretrieve('github release link to zip', 'temp_bundled_apks.zip')
-with zipfile.ZipFile('temp_bundled_apks.zip') as bnled_apks_zip
-    bndled_apks_zip.extractall('bundled_apks')
+with zipfile.ZipFile('temp_bundled_apks.zip') as tmp_b_a_zip
+    tmp_b_a_zip.extractall('bundled_apks')
 os.remove('temp_bundled_apks.zip')
 #done, installation can continue
 
